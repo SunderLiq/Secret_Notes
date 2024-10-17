232b5bad2dd38adb05df67850c92265520a2c449
SunderLiq <57959193+SunderLiq@users.noreply.github.com>
2024-10-17 21:25:06 +0300

    Sync with Firestore

--- app/src/main/java/app/compose/secretnotes/MainActivity.kt
+++ app/src/main/java/app/compose/secretnotes/MainActivity.kt
@@ -5,41 +5,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
-import app.compose.secretnotes.dataclasses.DataNote
 import app.compose.secretnotes.screens.main.Background
 import app.compose.secretnotes.screens.main.Header
 import app.compose.secretnotes.screens.main.NotesList
-import app.compose.secretnotes.ui.theme.NoteGreen
 import app.compose.secretnotes.ui.theme.SecretNotesTheme
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val fb = Firebase.firestore
+            fb.collection("Notes").document().set(mapOf("First note" to "text text text"))
             SecretNotesTheme {
                 Background()
                 Column {
                     Header()
-                    NotesList(listOf(
-                        DataNote(
-                            "Первая заметка",
-                            "Вот тут надо написать большой обширный текст" +
-                                    " Ваще пипец какой большой текст, я даже не знаю" +
-                                    " что тут написать. Ну просто пипец как много текста надо. " +
-                                    "Прям переносись на вторую и третью строчку, давай да ура.",
-                            "16.10.2024 21:08",
-                            "",
-                            NoteGreen
-                        ),
-                        DataNote(
-                            "Вторая заметка",
-                            "А вот это немного меньше, но не менее важная",
-                            "16.10.2024 19:32",
-                            "",
-                            NoteGreen
-                        )
-                    ))
+                    NotesList(listOf())
                 }
             }
         }
